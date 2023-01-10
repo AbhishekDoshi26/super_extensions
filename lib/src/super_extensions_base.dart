b1ad54999541d4f4e384af7286ef5c38ff32b6d1
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2023-01-10 23:40:46 +0530

    added screen height and screen width

--- lib/src/super_extensions_base.dart
+++ lib/src/super_extensions_base.dart
@@ -4,4 +4,12 @@ extension ContextExtension on BuildContext {
   void showSnackBar(SnackBar snackBar) {
     ScaffoldMessenger.of(this).showSnackBar(snackBar);
   }
+
+  double screenHeight() {
+    return MediaQuery.of(this).size.height;
+  }
+
+  double screenWidth() {
+    return MediaQuery.of(this).size.width;
+  }
 }
