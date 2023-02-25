57d40b089e4b4b957193fe4d8a6f42b82f5f262b
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2023-02-25 22:49:05 +0530

    added github actions

--- lib/src/super_extensions_base.dart
+++ lib/src/super_extensions_base.dart
@@ -140,10 +140,10 @@ extension StringExtension on String {
 
 extension NumberExtension on num {
   /// Creates an empty SizedBox with [height] as the given number.
-  /// 
+  ///
   SizedBox get hSizedBox => SizedBox(height: toDouble());
 
   /// Creates an empty SizedBox with [width] as the given number.
-  /// 
+  ///
   SizedBox get wSizedBox => SizedBox(width: toDouble());
 }
