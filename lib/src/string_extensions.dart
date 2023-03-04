ab5a11908d5f69ae482e226832a3ebc29db0b3bd
Bhoomit <bhfirebase74@gmail.com>
2023-03-04 11:01:59 +0530

    Add string extension for null or empty check.

--- lib/src/string_extensions.dart
+++ lib/src/string_extensions.dart
@@ -79,3 +79,13 @@ extension StringExtension on String {
     }
   }
 }
+
+extension StringExtensionWithNull on String? {
+  bool isNullOrEmpty() {
+    return this == null || this!.isEmpty;
+  }
+
+  bool isNotNullOrEmpty() {
+    return this != null && this!.isNotEmpty;
+  }
+}
