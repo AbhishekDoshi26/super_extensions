aa580decc6e9d772135d9391a2e37ebb760da84a
Ahmad jz <ahmadjzz99@gmail.com>
2023-03-18 12:30:51 +0300

    fix typo

--- lib/src/null_string_extensions.dart
+++ lib/src/null_string_extensions.dart
@@ -1,5 +1,5 @@
 extension NullStringExtension on String? {
-  ///Extension method on String? to return empty string if String value is null
+  ///Extension method on String? to return empty string if string's value is null
   String emptyStringIfNull() {
     if (this == null) {
       return "";
