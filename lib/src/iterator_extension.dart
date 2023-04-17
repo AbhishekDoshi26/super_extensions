42daf36fa449200c8b8d822bf1abdc85c6a78d88
Burhanuddin Rashid <burhanuddin@shiny.team>
2023-04-17 17:08:34 +0530

    Separator doc with example

--- lib/src/iterator_extension.dart
+++ lib/src/iterator_extension.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/widgets.dart';
 
-/// Extension on [Iterable] to add a separator between each widget using generators.
+/// Extension on Iterable to add a separator between each widget using generators.
 extension IterableExt on Iterable<Widget> {
+  ///Add separator widget in between the items.
+  ///It takes [element] as the parameter of type Widget.
+  ///Example to use these in the Column class:
+  ///```
+  ///Column(
+  ///  children: [
+  ///    const Text('T1'),
+  ///    const Text('T2'),
+  ///    const Text('T3'),
+  ///    const Text('T4'),
+  ///  ].separator(const SizedBox(height: 8.0)).toList(),
+  ///);
+  ///```
+  ///
   Iterable<Widget> separator(Widget element) sync* {
     final iterator = this.iterator;
     if (iterator.moveNext()) {
